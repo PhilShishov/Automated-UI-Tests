1ed2904ab47398042ebab110a479ec67bda695ff
Philip Shishov <philshishov@gmail.com>
2022-05-11 23:20:25 +0300

    Exam NetCore * net6

---
+++ Exams/Exam_NetCore/Pages/NetCorePage.cs
@@ -0,0 +1,14 @@
+﻿namespace Exam_NetCore.Pages
+{
+    using Exam_AutomateThePlanet.Pages;
+
+    using OpenQA.Selenium;
+
+    public partial class NetCorePage : BasePage
+    {
+        public NetCorePage(IWebDriver driver)
+            :base(driver)
+        {
+        }
+    }
+}
